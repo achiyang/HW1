c5b7fda0fbc7de10b5e43b42c04a181f1454db65
achi_yang <sinhouse2@gmail.com>
2023-09-08 22:10:08 +0900

    update HW70

--- HW70(echo)_145/HW70(echo)_145/Source.c
+++ HW70(echo)_145/HW70(echo)_145/Source.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 int main() {
-	int S_len, half_len, cnt = 0;
-	char* S;
+	int N, half_len = 0;
+	char S[101];
 
-	scanf("%d", &S_len);
+	scanf("%d %s", &N, S);
 
-	half_len = S_len / 2;
-
-	S = malloc(S_len + 1);
-
-	scanf("%s", S);
-
-	if (!(S_len & 1) && strncmp(S, &S[half_len], half_len) == 0) {
-		printf("%.*s", half_len, S);
+	if (N % 2) {
+		printf("NO");
 		return 0;
 	}
 
-	free(S);
+	half_len = N / 2;
 
-	printf("NO");
+	if (!strncmp(S, S + half_len, half_len)) {
+		printf("%s", S + half_len);
+	}
+	else {
+		printf("NO");
+	}
 
 	return 0;
 }
